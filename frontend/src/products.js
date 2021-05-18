5def97d2e24da9d2b6c240b1e84e3d2ac516578e
Logan Negley <logannegley@gmail.com>
2021-05-18 16:33:13 -0500

    added image and product objects for the shop

---
+++ frontend/src/products.js
@@ -0,0 +1,89 @@
+const products = [
+  {
+    id: "1",
+    name: "Blue Bird",
+    size: "11x14",
+    description: "Momma blue bird sitting on her nest in a tree",
+    category: "Birds",
+    price: "54.99",
+    countInStock: 50,
+    rating: 4.5,
+    numReviews: 4,
+  },
+
+  {
+    id: "2",
+    name: "Young Giraffe",
+    size: "11x14",
+    description: "Young giraffe frolicking in their habitat",
+    category: "Animals",
+    price: "49.99",
+    countInStock: 3,
+    rating: 4,
+    numReviews: 2,
+  },
+
+  {
+    id: "3",
+    name: "Vintage Camera Version One",
+    size: "8x10",
+    description: "Vintage camera with vintage surroundings",
+    category: "Product",
+    price: "25.99",
+    countInStock: 3,
+    rating: 5,
+    numReviews: 10,
+  },
+
+  {
+    id: "4",
+    name: "Palm Tree View of a Sunset",
+    size: "8x10",
+    description:
+      "A palm tree has the the best views of the sunset and ocean every evening",
+    category: "Sunset",
+    price: "69.99",
+    countInStock: 10,
+    rating: 5,
+    numReviews: 6,
+  },
+
+  {
+    id: "5",
+    name: "The Pink Flamingos",
+    size: "11x14",
+    description:
+      "A couple of flamingos enjoying eachother on the banks of a pond",
+    category: "Animals",
+    price: "42.99",
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 3,
+  },
+
+  {
+    id: "6",
+    name: "Pretty Bird",
+    size: "11x14",
+    description: "Beautiful Bird showing off their color and saying hi",
+    category: "Birds",
+    price: "79.99",
+    countInStock: 50,
+    rating: 5,
+    numReviews: 35,
+  },
+
+  {
+    id: "7",
+    name: "Vintage Camera Version Two",
+    size: "8x10",
+    description: "A vintage camera from the 1970s product shot",
+    category: "Product",
+    price: "15.99",
+    countInStock: 5,
+    rating: 4.0,
+    numReviews: 10,
+  },
+];
+
+export default products;
