e09e064b81907b6eaa6c095687d667b8d9bcc7d9
Logan Negley <logannegley@gmail.com>
2021-05-19 17:37:30 -0500

    set up dev dependencies to run both backend and frontend together with one script

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import products from "../products";
 import Product from "../components/Product";
 import { Row, Col } from "react-bootstrap";
 
--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Ratings";
-import products from "../products";
 
 const ProductPage = () => {
+  const [selectedProduct, setSelectedProduct] = useState({});
   const { id } = useParams();
-  const selectedProduct = products.find((product) => product._id === id);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/product/${id}`)
+      .then((res) => {
+        const { data } = res;
+        setSelectedProduct(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [id]);
 
   return (
     <>
