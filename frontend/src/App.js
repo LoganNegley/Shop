8fbf31137f85b159186c2e4ba44a7689056f9df8
Logan Negley <logannegley@gmail.com>
2021-05-19 12:07:10 -0500

    implementing Link and Link Container from react-router-dom and bootstrap on homePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
 // import logo from './logo.svg';
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <main className="py-3">
         <Container>
-          <HomePage />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/product/:id" component={ProductPage} />
         </Container>
       </main>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 };
 
