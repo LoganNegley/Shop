49cc6f961311d1415fa7cefbb4dde2b7e767f7df
Logan Negley <logannegley@gmail.com>
2021-05-20 13:21:49 -0500

    db config and connected to server

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv").config();
 const products = require("./data/products");
+const connectDB = require("./config/db");
+
+connectDB(); //Connecting to mongoose from db config
 
 const app = express();
 app.use(cors());
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server listening on ${port}`);
-});
-
+// Endpoints
 // Get all products
 app.get("/api/products", (req, res) => {
   res.status(200).json(products);
@@ -19,11 +20,14 @@ app.get("/api/products", (req, res) => {
 // Get product by ID
 app.get("/api/product/:id", (req, res) => {
   const { id } = req.params;
-
   const product = products.find((item) => item._id === id);
   res.status(200).json(product);
 });
 
+// Server Listening
+app.listen(port, () => {
+  console.log(`Server listening on ${port} in ${process.env.NODE_ENV} mode`);
+});
 // Testing server running
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Api up and running" });
