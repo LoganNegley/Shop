8846057639681751873024c0414778b45ea08008
Logan Negley <logannegley@gmail.com>
2021-05-23 17:15:13 -0500

    added sample data for users and installed bcrypt to hash example users passwords

---
+++ backend/data/users.js
@@ -0,0 +1,25 @@
+const bcrypt = require('bcryptjs');
+
+const users = [
+    {
+        name:'admin',
+        email:'admin@admin.com',
+        password:bcrypt.hashSync('123456', 10),
+        isAdmin:true
+    },
+
+    {
+        name:'test',
+        email:'test@test.com',
+        password:bcrypt.hashSync('123456', 10),
+        // isAdmin:false     false by default
+    },
+
+    {
+        name:'testUser',
+        email:'testUser@testUser.com',
+        password:bcrypt.hashSync('123456', 10),
+        // isAdmin:false    fasle by default
+    },
+
+module.exports = users;
