5e49700597f4ed1b10baf8a2896ec5f70cc28447
Logan Negley <logannegley@gmail.com>
2021-05-24 14:14:48 -0500

    custom middleware created and used- error handling and route not found middleware

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,25 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
-const products = require("./data/products");
 const connectDB = require("./config/db");
+const productRoutes = require("./routes/productRoutes");
+const {routeNotFound, errorHandler} = require('./middleware/errorMiddleware');
+
 
 connectDB(); //Connecting to MongoDb with mongoose from db config
 
 const app = express();
 app.use(cors());
 
+app.use("/api/products", productRoutes);
+
+// Middleware
+app.use(routeNotFound);
+app.use(errorHandler);
+
 const port = process.env.PORT || 5000;
 
 // Endpoints
-// Get all products
-app.get("/api/products", (req, res) => {
-  res.status(200).json(products);
-});
-
-// Get product by ID
-app.get("/api/product/:id", (req, res) => {
-  const { id } = req.params;
-  const product = products.find((item) => item._id === id);
-  res.status(200).json(product);
-});
-
-// Server Routes
 // Server Listening
 app.listen(port, () => {
   console.log(`Server listening on ${port} in ${process.env.NODE_ENV} mode`);
