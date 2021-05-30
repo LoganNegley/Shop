ff8c0d0946dc8095423b01f1937c5b6e88a3b5b7
Logan Negley <logannegley@gmail.com>
2021-05-30 14:32:46 -0500

    cart action and reducers implemented

--- frontend/src/state/reducers/cartReducers.js
+++ frontend/src/state/reducers/cartReducers.js
@@ -1,24 +1,24 @@
-export cartReducer (state ={cartItems:[]}, action) =>{
+export const cartReducer = (state ={cartItems:[]}, action) =>{
     switch (action.type) {
         case 'CART_ADD_ITEM':
             const item = action.payload;
-            const exist = state.cartItems.find(x =>{
-                x.product === item.product.id
-            })
-            if(exist){
+            const exist = state.cartItems.find(x =>(  //check if item is already in the cart or not
+                x.product_id === item.product_id
+            ))
+            if(exist){  //if item is in cart already????
                 return{
-                    ...state,
-                    cartItems: state.cartItems.map(x => {
-                        x.product === exist.product ? item : x
-                    })
+                    ...state, //spread all current state items
+                    cartItems: state.cartItems.map(x =>(    //go through all state.cartItems
+                        x.product_id === exist.product_id ? item : x
+                    ))
                 }
-            }else{
+            }else{  //if item not in cart already
                 return {
-                    ...state,
-                    cartItems:[...state.cartItems, item]
+                    ...state, //return current state 
+                    cartItems:[...state.cartItems, item] //state.cartItems spread current state array add new item
                 }
             }
-            return {cartItems:[...cartItems, action.payload]}
+            // return {cartItems:[...cartItems, action.payload]}
     
         default:
             return state;
--- frontend/src/state/store.js
+++ frontend/src/state/store.js
@@ -1,14 +1,21 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-import {composeWithDevTools} from 'redux-devtools-extension'; //have to use with chrome extension
+import {composeWithDevTools} from 'redux-devtools-extension'; //have to use with redux chrome extension
 import {productListReducer, productDetailsReducer} from './reducers/productReducers';
+import {cartReducer} from './reducers/cartReducers';
 
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    cart: cartReducer,
 });
-const initialState = {};
+
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+
+const initialState = {
+    cart: {cartItems:cartItemsFromStorage}
+};
 const middleware = [thunk];
 
 const store = createStore(
