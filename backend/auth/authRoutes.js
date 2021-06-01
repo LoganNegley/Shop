fcc44791e07396a4f362a8574d7eb4868187799f
Logan Negley <logannegley@gmail.com>
2021-06-01 16:03:26 -0500

    auth login routes

--- backend/auth/authRoutes.js
+++ backend/auth/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const User = require('../models/userModel');
-
+const bcrypt = require('bcryptjs');
 
 const router = express.Router();
 
@@ -11,8 +11,17 @@ router.post('/login', (req,res) =>{
     
     User.findOne({email:email})
     .then(user =>{
-        
-        console.log(user)
+        if(user && bcrypt.compareSync(password, user.password)){
+            res.status(200).json({
+                _id:user._id,
+                name:user.name,
+                email:user.email,
+                isAdmin:user.isAdmin,
+                token: null
+            })
+        }else{
+            res.status(401).json({message: 'Invalid username or password'})
+        }
     })
     .catch(error =>{
         res.status(500).json({errorMessage:'Unable to find user'})
